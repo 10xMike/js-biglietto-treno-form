6a8a7639d2f60ba42c0a95412dc1b8814c39da36
10xMike <michelegec@gmail.com>
2021-05-03 23:25:06 +0200

    Inseriti primi elementi nel main JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-//
+/* 
+Creiamo un finto biglietto del treno con:
+    Nome passeggero
+    Codice treno (numero casuale tra 90000 e 100000 escluso)
+    Numero carrozza
+    Prezzo calcolato
+    Categoria selezionata dall'utente
+*/
+
+var nomePasseggero = document.getElementById('nomePasseggero').value;
+console.log(nomePasseggero);
+var kmPercorsi = document.getElementById('kmPercorsi').value;
+console.log(kmPercorsi);
+var codiceCP;
+var nCarrozza;
+var prezzo;
+var fasciaEtà;
+
+
